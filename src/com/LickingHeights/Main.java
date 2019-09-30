7b73591918ba42fc5fe669fec14ae4a7474ec76a
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-30 09:50:27 -0400

    Coded intro to the Magic 8Ball

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,24 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //declare phase
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        Double number;
+
+
+
+
+        System.out.println("This is the Magic 8Ball.");
+        System.out.println("Insert a number from the range of 1 to 100.");
+        System.out.println("Once you insert a number you'll get the answer to every single one of your questions.");
+
+
+
+
+    }
+}
